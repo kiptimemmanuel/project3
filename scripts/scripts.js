ec524ccf804f254885eba4487ba53938237b626a
kiptimemmanuel <kiptimemmanuel@gmail.com>
2021-11-19 13:40:58 +0300

    added js function alert

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+function fn(){
+    let nameInput = document.getElementById("name").value;
+    let emailInput =  document.getElementById("email").value;
+    let commentInput =  document.getElementById("comment").value;
+    console.log (nameInput, emailInput, commentInput )
+    alert( nameInput +" we have received your message. Thank you for reaching out to us" )
+}
